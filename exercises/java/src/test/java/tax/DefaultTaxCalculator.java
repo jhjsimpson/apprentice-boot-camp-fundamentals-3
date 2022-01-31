d263b365832798c87866376a5cf50329a0b86624
Mark Crossfield <Mark.Crossfield@autotrader.co.uk>
2022-01-31 16:06:42 +0000

    Rebase Continue

--- exercises/java/src/test/java/tax/DefaultTaxCalculator.java
+++ exercises/java/src/test/java/tax/DefaultTaxCalculator.java
@@ -6,10 +6,37 @@ import static tax.FuelType.PETROL;
 public class DefaultTaxCalculator extends TaxCalculator {
 
     public int calculateTax(Vehicle vehicle) {
+        int emissions = vehicle.getCo2Emissions();
 
-        if(vehicle.getFuelType() == PETROL) {
+        if (vehicle.getFuelType() == FuelType.ALTERNATIVE_FUEL) {
+            if (emissions <= 50) {
+                return 0;
+            } else if (emissions <= 75) {
+                return 15;
+            } else if (emissions <= 90) {
+                return 95;
+            } else if (emissions <= 100) {
+                return 115;
+            } else if (emissions <= 110) {
+                return 135;
+            } else if (emissions <= 130) {
+                return 155;
+            } else if (emissions <= 150) {
+                return 195;
+            } else if (emissions <= 170) {
+                return 505;
+            } else if (emissions <= 190) {
+                return 820;
+            } else if (emissions <= 225) {
+                return 1230;
+            } else if (emissions <= 255) {
+                return 1750;
+            }
+            return 2060;
+        }
 
-            int emissions = vehicle.getCo2Emissions();
+        if (vehicle.getFuelType() == PETROL) {
+
             if (emissions >= 1 && emissions <= 50) {
                 return 10;
             }
@@ -48,9 +75,8 @@ public class DefaultTaxCalculator extends TaxCalculator {
             }
         }
 
-        if(vehicle.getFuelType() == DIESEL) {
+        if (vehicle.getFuelType() == DIESEL) {
 
-            int emissions = vehicle.getCo2Emissions();
             if (emissions >= 1 && emissions <= 50) {
                 return 25;
             }
@@ -88,7 +114,6 @@ public class DefaultTaxCalculator extends TaxCalculator {
                 return 2070;
             }
         }
-
-    return 0;
+        return 0;
     }
 }
